e4e9c494a9ab2793b34102222c3e9c1040d76417
Laure-Hélène Bruneton <lhbruneton.pro@gmail.com>
2021-03-13 13:11:57 +0100

    test(domains-cards.effects): inject mock selectors only when needed

--- libs/data-access-game/src/lib/+state/domains-cards/domains-cards.effects.spec.ts
+++ libs/data-access-game/src/lib/+state/domains-cards/domains-cards.effects.spec.ts
@@ -1,7 +1,8 @@
+import { Injector } from '@angular/core';
 import { TestBed } from '@angular/core/testing';
 import { provideMockActions } from '@ngrx/effects/testing';
 import { Action } from '@ngrx/store';
-import { provideMockStore } from '@ngrx/store/testing';
+import { MockStore, provideMockStore } from '@ngrx/store/testing';
 import { DataPersistence, NxModule } from '@nrwl/angular';
 import { hot } from '@nrwl/angular/testing';
 import { ID_DOMAIN_BLUE, ID_DOMAIN_RED } from '@taormina/shared-constants';
@@ -23,6 +24,7 @@ jest.mock('./domains-cards.models', () => {
 });
 
 describe('DomainsCardsEffects', () => {
+  let injector: Injector;
   let actions: Observable<Action>;
   let effects: DomainsCardsEffects;
 
@@ -33,41 +35,7 @@ describe('DomainsCardsEffects', () => {
         DomainsCardsEffects,
         DataPersistence,
         provideMockActions(() => actions),
-        provideMockStore({
-          selectors: [
-            {
-              selector: DomainsCardsSelectors.getLandCardsPivotsForDie,
-              value: [
-                {
-                  id: 'AAA',
-                  domainId: ID_DOMAIN_RED,
-                  cardType: AGGLOMERATION_CARD_INTERFACE_NAME,
-                  cardId: 'ROAD_1',
-                },
-                {
-                  id: 'BBB',
-                  domainId: ID_DOMAIN_BLUE,
-                  cardType: LAND_CARD_INTERFACE_NAME,
-                  cardId: 'LAND_1',
-                  value: 0,
-                },
-                {
-                  id: 'CCC',
-                  domainId: ID_DOMAIN_RED,
-                  cardType: LAND_CARD_INTERFACE_NAME,
-                  cardId: 'LAND_2',
-                  value: 3,
-                },
-                {
-                  id: 'DDD',
-                  domainId: ID_DOMAIN_BLUE,
-                  cardType: LAND_CARD_INTERFACE_NAME,
-                  cardId: 'LAND_3',
-                },
-              ],
-            },
-          ],
-        }),
+        provideMockStore(),
       ],
     });
 
@@ -105,6 +73,49 @@ describe('DomainsCardsEffects', () => {
   });
 
   describe('increaseResourceValue$', () => {
+    beforeEach(() => {
+      injector = Injector.create({
+        providers: [
+          provideMockStore({
+            selectors: [
+              {
+                selector: DomainsCardsSelectors.getLandCardsPivotsForDie,
+                value: [
+                  {
+                    id: 'AAA',
+                    domainId: ID_DOMAIN_RED,
+                    cardType: AGGLOMERATION_CARD_INTERFACE_NAME,
+                    cardId: 'ROAD_1',
+                  },
+                  {
+                    id: 'BBB',
+                    domainId: ID_DOMAIN_BLUE,
+                    cardType: LAND_CARD_INTERFACE_NAME,
+                    cardId: 'LAND_1',
+                    value: 0,
+                  },
+                  {
+                    id: 'CCC',
+                    domainId: ID_DOMAIN_RED,
+                    cardType: LAND_CARD_INTERFACE_NAME,
+                    cardId: 'LAND_2',
+                    value: 3,
+                  },
+                  {
+                    id: 'DDD',
+                    domainId: ID_DOMAIN_BLUE,
+                    cardType: LAND_CARD_INTERFACE_NAME,
+                    cardId: 'LAND_3',
+                  },
+                ],
+              },
+            ],
+          }),
+        ],
+      });
+      injector.get(MockStore);
+    });
+
     it('should work', () => {
       actions = hot('-a-|', {
         a: DomainsCardsActions.increaseLandValueForDie({ die: 1 }),
