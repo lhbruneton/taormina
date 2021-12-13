8fda163c48e97433753d754870489b1f8c37c6ab
Laure-Hélène Bruneton <lhbruneton.pro@gmail.com>
2021-12-13 20:53:09 +0100

    feat(ui-card): nx generate and fix component card

--- libs/ui-card/src/lib/ui-card.module.ts
+++ libs/ui-card/src/lib/ui-card.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { CardComponent } from './card/card.component';
+
 @NgModule({
   imports: [CommonModule],
+  declarations: [CardComponent],
 })
 export class UiCardModule {}
