b4f23ae6529e07ce614ac10a9d6b4b5b77eafb24
Laure-Hélène Bruneton <lhbruneton.pro@gmail.com>
2022-08-17 17:22:56 +0200

    refactor(ui-card): clear unused component

--- libs/ui-card/src/lib/card/card.component.ts
+++
@@ -1,7 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'taormina-card',
-  templateUrl: './card.component.html',
-})
-export class CardComponent {}
--- libs/ui-card/src/lib/ui-card.module.ts
+++ libs/ui-card/src/lib/ui-card.module.ts
@@ -1,13 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { CardComponent } from './card/card.component';
 import { CardBackModule } from './card-back/card-back.module';
 import { CardFrontModule } from './card-front/card-front.module';
 
 @NgModule({
   imports: [CommonModule, CardBackModule, CardFrontModule],
-  declarations: [CardComponent],
   exports: [CardBackModule, CardFrontModule],
 })
 export class UiCardModule {}
